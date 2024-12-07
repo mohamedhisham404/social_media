43fd8581675c7149b7569132878f3b3e28be71e8
mohamedhisham404 <mohamedhisham694@gmail.com>
2024-12-07 13:49:55 +0200

    chat models

---
+++ server/models/convesationModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ConversationSchema = mongoose.Schema(
+    {
+        participants: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }],
+        lastMessage: {
+            text:String,
+            sender:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        },
+    },
+    { timestamps: true }
+);
+
+const Conversation = mongoose.model('Conversation', ConversationSchema);
+
+export default Conversation;
